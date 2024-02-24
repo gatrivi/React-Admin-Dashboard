55b7d6bc2ed7ee39cf1f4442e731b7303052bd08
Developer Formerly Known as Decat <62384085+gatrivi@users.noreply.github.com>
2024-02-24 10:48:21 -0300

    Added Employees
    Used entire component from orders with a search and replace. Match case so that order became employee and Order, Employee, and so on. Pagination and sorting work.

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+
+import { employeesData, contextMenuItems, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Employees = () => {
-  return <div>Employees</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white  rounded-3xl">
+      AA
+      <Header category="Page" title="Employees" />
+      AA
+      <GridComponent
+        id="gridcomp"
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Employees;
