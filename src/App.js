06b819e73d74bd5c0007fe73dc14a68f923bdfc8
Developer Formerly Known as Decat <62384085+gatrivi@users.noreply.github.com>
2024-02-26 04:57:19 -0300

    kanban component main functionality implemented
    keyField is similar to the key field in react

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ const App = () => {
 
                 {/* Apps */}
 
-                <Route path="/kanban" element={<Line />} />
+                <Route path="/kanban" element={<Kanban />} />
                 <Route path="/editor" element={<Editor />} />
                 <Route path="/calendar" element={<Calendar />} />
                 <Route path="/color-picker" element={<ColorPicker />} />
