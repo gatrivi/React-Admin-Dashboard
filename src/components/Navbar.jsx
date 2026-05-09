2223b685e9e0fb31278fec7925e6c6353a0c4681
gatrivi <gatrivi@gmail.com>
2026-05-09 14:55:15 -0300

    chore: fix ESLint warnings and remove unused code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { RiNotification3Line } from "react-icons/ri";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import avatar from "../data/avatar.jpg";
-import { Cart, Chat, Menu, Notification, UserProfile } from ".";
+import { Cart, Chat, Notification, UserProfile } from ".";
 
 import { useStateContext } from "../context/ContextProvider";
 
@@ -27,11 +27,10 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
   </TooltipComponent>
 );
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { setActiveMenu } = useStateContext();
 
   const {
     isClicked,
-    setIsClicked,
     screenSize,
     setScreenSize,
     handleClick,
@@ -46,7 +45,7 @@ const Navbar = () => {
     handleResize();
 
     return () => window.removeEventListener("resize", handleResize);
-  });
+  }, [setScreenSize]);
 
   useEffect(() => {
     if (screenSize <= 900) {
@@ -54,7 +53,7 @@ const Navbar = () => {
     } else {
       setActiveMenu(true);
     }
-  }, [screenSize]);
+  }, [screenSize, setActiveMenu]);
 
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -9,7 +9,7 @@ import {
   Inject,
 } from "@syncfusion/ej2-react-grids";
 
-import { employeesData, contextMenuItems, employeesGrid } from "../data/dummy";
+import { employeesData, employeesGrid } from "../data/dummy";
 import { Header } from "../components";
 
 const Employees = () => {
